f3ece4facbd8dae697d3427a3642ceb7881e19cc
Sachint <Sachin.tatrari2106@gmail.com>
2025-08-14 10:24:17 +0530

    Pushing the node application

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.send("You are seeing the application live");
+})
+
+app.listen(3000, () =>{
+    console.log('Server is running on port 3000')
+})
+
+module.exports= app;
